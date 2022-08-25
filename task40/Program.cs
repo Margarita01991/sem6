f6414812d3c3561aba1d7a577b8404d0885643e9
Margarita <margarita27011991@gmail.com>
2022-08-25 10:59:54 +0300

    рекурсия, получение двоичного числа

---
+++ task40/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины
+//"каждая сторона треугольника меньше суммы двух других сторон"
+int[] FillArray (int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int [size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int[] array = FillArray(3, 1, 100);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+int result =0;
+if (array[0] < array[1]+array[2]) 
+    {
+        if (array[1] < array[0]+array[2])
+        {
+            if (array[2] < array[0]+array[1]) result = 1;
+        }
+    }
+else result = 0;
+if (result == 1) Console.WriteLine("да");
+else Console.WriteLine("нет");
---
+++ task42/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа, которая будет преобразовывать десятичное число в двоичное
+/* STRING
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+string result = String.Empty;
+while (n !=0)
+{
+    result+=Convert.ToString(n % 2);
+    n = n / 2;
+}
+Console.Write(result);
+*/
+
+/* FOR
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+// int d = 0;
+
+for (int i = 0; n >= 1; i++) // FOR (инициализация счетчика I; проверка условия I<COUNT; инкремент i++)
+{
+    d = n % 2;
+    n = n / 2;
+    Console.Write(d);
+}
+*/
+/* РЕКУРСИЯ*/
+void PrintBinary(int N)                                          // вызываем метод, передаем наше число n == 45                                         
+{                                                                // 
+    if (N <= 0) return;// при каком условии точка остановки      
+    PrintBinary(N / 2);                                          // пока условие N>=0 выполняется, метод зациклен, выполняется 7 заходов. Как только N < 0, функция останавливается идет обратный reverse 
+    Console.Write(N % 2);                                        // и записывается остаток от деления в обратном порядке
+}
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintBinary(n); //вызываем метод
